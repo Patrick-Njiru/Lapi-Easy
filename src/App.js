9a09bcff10269e3bc71d7fb6ba8c7564b0c81f6e
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-05 14:25:00 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  
+  const [laptops, setLaptops] = useState([])
+  const API = 'https://patrick-ecommerce-server.herokuapp.com/laptops'
+
+  useEffect(() => {
+    fetch(API).then(res=> res.json()).then(data => setLaptops(data)).catch(console.log)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello World!</p>
     </div>
   );
 }
