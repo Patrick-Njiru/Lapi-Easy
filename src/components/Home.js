f639a507ea72c2a77e6945f1621aa539414bb5de
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-05 23:12:16 +0300

    Modify file

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
         <br /><br /><br />
         <main className='about'>
             <h2>About</h2>
-            <h3>A platform designed primarily for college students to give them an easier time when searching for suitable laptops for their academic studies at affordable prices based on their preferences. The students also get a chance to negotiate prices with sellers and post their own laptops that they may want to sell.</h3>
+            <h3>A platform designed primarily for college students to give them an easier time when searching for suitable laptops for their academic studies at affordable prices based on their preferences. Apart from a wide variety of laptops, the students also get a chance to negotiate prices with sellers and post their own laptops that they may want to sell.</h3>
         </main>
     </div>
   )
