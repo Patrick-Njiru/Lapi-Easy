98055b1edc96c1afc5c0d547d05d9d5769016873
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-05 22:58:11 +0300

    Add elements.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <br /><br /><br />
+        <main className='about'>
+            <h2>About</h2>
+            <h3>A platform designed primarily for college students to give them an easier time when searching for suitable laptops for their academic studies at affordable prices based on their preferences. The students also get a chance to negotiate prices with sellers and post their own laptops that they may want to sell.</h3>
+        </main>
+    </div>
   )
 }
 
