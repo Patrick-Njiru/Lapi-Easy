0186aa57501b0b414804a6b4f87ad7f9d8466aa3
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-06 11:54:53 +0300

    Use prop to obtain data from server and incorporate it into the component.

--- src/components/Laptops.js
+++ src/components/Laptops.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import LaptopDetails from './LaptopDetails'
 
-function Laptops() {
-  return (
-    <div>
-        <LaptopDetails />
-        <h1>Laptops</h1>
+function Laptops({ laptops }) {
+    
+    const laptop = laptops.map(laptop => (
+        <main key={laptop.id}>
+            <img src={laptop.image} alt={laptop.name} />
+            <h4> 
+                <span>{laptop.name}</span>
+                <span>Ksh. {laptop.price}</span>
+                <span>{laptop.specifications.condition}</span>
+            </h4>
+            <div className='buttons'>
+            <button>save / unsave</button>
+            <button>More details</button>
+            </div>
+        </main>
+     ))
 
+  return (
+    <div className='laptops'>
+        {/* <LaptopDetails /> */}
+        <h1>Available products</h1>
+        <div className='generalDetails'>{laptop}</div>
     </div>
   )
 }
