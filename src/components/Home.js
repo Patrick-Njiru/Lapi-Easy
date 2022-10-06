4e547c88b79f1d5c87bd0aa9167300588eada4b1
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-06 11:53:04 +0300

    Import and implement NavLink hook, and add icon.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from "react-router-dom"
 
 function Home() {
   return (
@@ -8,6 +9,10 @@ function Home() {
             <h2>About</h2>
             <h3>A platform designed primarily for college students to give them an easier time when searching for suitable laptops for their academic studies at affordable prices based on their preferences. Apart from a wide variety of laptops, the students also get a chance to negotiate prices with sellers and post their own laptops that they may want to sell.</h3>
         </main>
+        <NavLink className='products' to="/laptops">
+            View Laptops &nbsp;
+            <i class="fa-solid fa-arrow-right"></i>
+        </NavLink>
     </div>
   )
 }
