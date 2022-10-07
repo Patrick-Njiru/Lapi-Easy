b00e042d4503b55062e42e6560002790d05fb161
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-07 12:30:11 +0300

    Update form component by adding and using useState hook, attributes to components and functions to handle entered data.

--- src/components/Sell.js
+++ src/components/Sell.js
@@ -1,57 +1,210 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Sell() {
+function Sell({ setLaptops, API }) {
+  const [formData, setFormData] = useState({
+    sellerContact: null,
+    sellerStoreAddress: "",
+    model: "",
+    image: "",
+    price: null,
+    operatingSystem: "",
+    ram: '',
+    processor: '',
+    graphicsCard: '',
+    storageType: '',
+    storageCapacity: '',
+    displaySize: '',
+    color: '',
+    condition: ''
+  })
+
+  const handleChange = e => setFormData({ ...formData, [e.target.name]: e.target.value})
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const updatedForm = {
+      sellerContact: formData.sellerContact,
+      sellerStoreAddress: formData.sellerStoreAddress,
+      model: formData.model,
+      image: formData.image,
+      price: formData.price,
+      operatingSystem: formData.operatingSystem,
+      ram: formData.ram,
+      processor: formData.processor,
+      graphicsCard: formData.graphicsCard,
+      storageType: formData.storageType,
+      storageCapacity: formData.storageCapacity,
+      displaySize: formData.displaySize,
+      color: formData.color,
+      condition: formData.condition
+    }
+
+    fetch(API, {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(updatedForm)
+    })
+    .then(res => res.json()).then(laptops => setLaptops(laptops))
+    .catch(console.log)
+
+    setFormData({
+      sellerContact: null,
+      sellerStoreAddress: "",
+      model: "",
+      image: "",
+      price: null,
+      operatingSystem: "",
+      ram: '',
+      processor: '',
+      graphicsCard: '',
+      storageType: '',
+      displaySize: '',
+      color: '',
+      condition: ''
+    })
+  }
+
   return (
     <div className='sell'>
       <h2>Fill the following details about your product and submit to post it. </h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <section>
             <h3>Your Details</h3>
               <label> 
-                Contact : <input type='tel' required />
+                Contact(format - 07XXXXXXXX / +2547XXXXXXXX) : 
+                <input 
+                type='tel' 
+                minLength='10'
+                name='sellerContact' 
+                required 
+                onChange={handleChange}
+                />
               </label> <br />
               <label> 
-                Store Address : <input type='text' />
+                Store Address : 
+                <input 
+                name='sellerStoreAddress' 
+                type='text' 
+                onChange={handleChange} 
+                />
               </label>
           </section>
           <section>
               <h3>Laptop Details</h3>
               <label> 
-                Picture : <input type='file' required />
+                Name / Model : 
+                <input 
+                type='text' 
+                name='model' 
+                required 
+                onChange={handleChange}
+                />
+              </label> <br />
+              <label> 
+                Picture : 
+                <input 
+                type='file' 
+                name='image' 
+                required 
+                onChange={handleChange} 
+                />
               </label> <br />
               <label>
-                Price : <input type='number' required />  <br />
-              </label> 
+                Price(Ksh) : 
+                <input 
+                type='number' 
+                name='price' 
+                required 
+                onChange={handleChange} 
+                />
+              </label> <br />
               <label>
-                Operating System : <input type='text' required />
+                Operating System : 
+                <input 
+                type='text'
+                name='operatingSystem' 
+                required 
+                onChange={handleChange} 
+                />
               </label> <br />
               <label> 
-                RAM : <input type='text' required />
+                RAM : 
+                <input 
+                type='text'
+                name='ram' 
+                required 
+                onChange={handleChange} 
+                />
               </label> <br />
               <label> 
-                Processor : <input type='text' required />
+                Processor : 
+                <input 
+                type='text'
+                name='processor' 
+                required 
+                onChange={handleChange} 
+                />
               </label> <br />
               <label> 
-                Graphics Card : <input type='text' required />
+                Graphics Card : 
+                <input 
+                type='text'
+                name='graphicsCard' 
+                required 
+                onChange={handleChange} 
+                />
               </label> <br />
               <label> 
-                Storage Type : <input type='text' required />
+                Storage Type : 
+                <input 
+                type='text'
+                name='storageType' 
+                required 
+                onChange={handleChange} 
+                />
               </label> <br />
               <label> 
-                Storage Capacity : <input type='text' required />
+                Storage Capacity : 
+                <input 
+                type='text'
+                name='storageCapacity' 
+                required 
+                onChange={handleChange} 
+                />
               </label> <br />
               <label> 
-                Display Size(inch) : <input type='text' required />
+                Display Size(inch) : 
+                <input 
+                type='text'
+                name='displaySize' 
+                required 
+                onChange={handleChange} 
+                />
               </label> <br />
               <label> 
-                Color : <input type='text' />
+                Color : 
+                <input 
+                type='text'
+                name='color' 
+                onChange={handleChange}
+                 />
               </label> <br />
               <label> 
-                Condition : <input type='text' required />
+                Condition of Laptop: 
+                <input 
+                type='text'
+                name='condition' 
+                onChange={handleChange} 
+                />
               </label>
           </section>
-          <input id='submit' type='submit' value='Post' />
+          <input 
+          id='submit' 
+          type='submit' 
+          value='Post'  
+          />
         </div>
       </form>
     </div>
