4554923791555462d6230446e415e3ba07304677
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-08 12:55:01 +0300

    Add state, modify UseEffect hook, and create functions and pass them as props to children components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Home from './Home';
-import Laptops from './Laptops';
+import LaptopCollection from './LaptopCollection';
 import NavBar from './NavBar';
 import Saved from './Saved';
 import Sell from './Sell';
@@ -11,19 +11,37 @@ import {Routes, Route} from "react-router-dom"
 function App() {
   
   const [laptops, setLaptops] = useState([])
+  const [savedLaptops, setSavedLaptops] = useState([])
+
+
   const API = 'https://patrick-ecommerce-server.herokuapp.com/laptops' // my deployed json-server
 
   useEffect(() => {
-    fetch(API).then(res=> res.json()).then(data => setLaptops(data)).catch(console.log)
-  }, [laptops])
+    console.log("UseEffect called");
+    fetch(API).then(console.log("Loading ...")).then(res=> res.json()).then(data => setLaptops(data)).catch(console.log)
+  }, [])
+
+  const saveLaptop = id => setSavedLaptops(laptops.map(laptop => id === laptop.id ? {...laptop, isSaved: true}: laptop ))
+  const removeSavedLaptop = id => setSavedLaptops(laptops.filter(laptop => id === laptop.id ))
 
   return (
     <div>
       <NavBar />
       <Routes>
         <Route exact path='/' element={<Home />} />
-        <Route path='/laptops' element={<Laptops laptops={laptops} />} />
-        <Route path='/saved' element={<Saved />} />
+        <Route 
+          path='/laptops' 
+          element={<LaptopCollection handleClick={saveLaptop} laptops={laptops} />} 
+        />
+        <Route 
+          path='/saved' 
+          element={
+            <Saved 
+              handleClick={removeSavedLaptop} 
+              laptops={laptops.filter(laptop => laptop.saved)} 
+            />
+          } 
+        />
         <Route path='/sell' element={<Sell API={API} setLaptops={setLaptops} />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
